749b503c599622de77e93962d67de05e1a083e9a
Aleksa Vukadinovic <alexvukadin8@gmail.com>
2026-01-30 22:43:03 +0100

    update gitignore and add script for deleting .out files

---
+++ clear_outputs.py
@@ -0,0 +1,19 @@
+import os
+
+EXTENSIONS_TO_DELETE = {".out"}
+
+def delete_output_files(path):
+    for entry in os.listdir(path):
+        full_path = os.path.join(path, entry)
+
+        if os.path.isfile(full_path):
+            _, ext = os.path.splitext(entry)
+            if ext in EXTENSIONS_TO_DELETE:
+                os.remove(full_path)
+        elif os.path.isdir(full_path):
+            delete_output_files(full_path)
+
+if __name__ == "__main__":
+    print("Deleting .out files...")
+    delete_output_files(".")
+    print("Deletion successful")
